4052d7ee1ca8bd010df32e62fa69c51b0a81222c
ovhanago <ovhan.ago15@gmail.com>
2026-07-15 18:33:11 -1200

    Update laporan.php

--- laporan.php
+++ laporan.php
@@ -194,10 +194,10 @@ if ($filter != 'all') {
                                     <td class="fw-bold text-primary">Rp <?= number_format($row['total_harga'], 0, ',', '.') ?></td>
                                     <td>
                                         <?php 
-                                       $status_class = 'bg-dark text-white';
-                                        if($row['status_pesanan'] == 'Success' || $row['status_pesanan'] == 'Selesai') $status_class = 'bg-dark text-white';
-                                        elseif($row['status_pesanan'] == 'Pending') $status_class = 'bg-dark text-white';
-                                        elseif($row['status_pesanan'] == 'Failed') $status_class = 'bg-dark text-white';
+                                        $status_class = 'bg-secondary text-white';
+                                        if($row['status_pesanan'] == 'Success' || $row['status_pesanan'] == 'Selesai') $status_class = 'bg-success text-white';
+                                        elseif($row['status_pesanan'] == 'Pending') $status_class = 'bg-warning text-dark';
+                                        elseif($row['status_pesanan'] == 'Failed') $status_class = 'bg-danger text-white';
                                         ?>
                                         <span class="badge badge-status <?= $status_class ?>"><?= $row['status_pesanan'] ?></span>
                                     </td>
